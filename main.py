7e4ab6c4d6a4bad2276320cd1cfaeacde6f069bd
Александр Петров <sasa9089@yandex.ru>
2022-11-29 17:27:08 +0300

    added a module

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name:} нанёс противнику урон, равный '
@@ -79,6 +82,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,3 +92,6 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
+
+main()
